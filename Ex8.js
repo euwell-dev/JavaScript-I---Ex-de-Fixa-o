0171ecc46a52daacaaa5cf27a7a9bf6334f83b1d
euwell-dev <130098196+euwell-dev@users.noreply.github.com>
2023-04-24 18:41:09 -0300

    Create Ex8.js

---
+++ Ex8.js
@@ -0,0 +1,20 @@
+
+const peso = parseFloat(prompt("Digite o seu peso:"));
+const altura = parseFloat(prompt("Digite a sua altura:"));
+const imc = peso / (altura*altura);
+
+
+let condicao;
+if (imc < 18.5) {
+  condicao = "Abaixo do peso";
+} else if (imc >= 18.5 && imc < 25) {
+  condicao = "Peso normal";
+} else if (imc >= 25 && imc < 30) {
+  condicao = "Acima do peso";
+} else {
+  condicao = "Obeso";
+}
+
+document.write("<p>"+ "Seu peso é"+" "+peso.toFixed(2)+"</p>");
+document.write("<p>"+ "Sua altura é"+" "+altura.toFixed(2)+"</p>");
+document.write("<p>"+ "Sua condição é"+" "+"<u>"+condicao+"<u>"+"."+ "</p>");
