3959491763618b6c1ce7c6fa91da1ec75230874b
euwell-dev <130098196+euwell-dev@users.noreply.github.com>
2023-04-24 17:55:50 -0300

    Create Ex5.js

---
+++ Ex5.js
@@ -0,0 +1,9 @@
+const nomedoproduto = prompt("Digite o nome do produto:");
+const preco = prompt("Digite o preço produto:");
+const estoque = prompt("Digite a quantidade do estoque:");
+const validade = prompt("Digite a validade do produto:");
+
+document.write("<h3>"+ "Produto:"+ " "+nomedoproduto+"</h3>");
+document.write("<p>"+ "Preço R$:"+ " "+preco+"</p>");
+document.write("<p>"+ "Estoque:"+ " "+nomedoproduto+"</p>");
+document.write("<p>"+ "Validade:"+ " "+nomedoproduto+"</p>");
