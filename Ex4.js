7ddbd892902cfe904a3aa6c0f629f002a6b60859
euwell-dev <130098196+euwell-dev@users.noreply.github.com>
2023-04-24 17:53:12 -0300

    Create Ex4.js

---
+++ Ex4.js
@@ -0,0 +1,10 @@
+
+const nome = prompt("Digite o seu nome:");
+const cpf = prompt("Digite o seu cpf:");
+const idade = prompt("Digite a sua idade:");
+const data_nascimento = prompt("Digite sua data de nascimento:");
+
+console.log("o seu nome é: " + nome)
+console.log("o seu CPF é: " + cpf)
+console.log("a sua idade é: " + idade)
+console.log("A sua data de nasciemnto é: " + data_nascimento)
