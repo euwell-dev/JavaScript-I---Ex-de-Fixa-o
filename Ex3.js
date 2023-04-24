f0b06bfb81bd4bb07d0ab7eb8a99292b31f1177f
euwell-dev <130098196+euwell-dev@users.noreply.github.com>
2023-04-24 13:57:35 -0300

    Create Ex3.js

---
+++ Ex3.js
@@ -0,0 +1,25 @@
+const numero1 = parseInt(prompt("Digite o primeiro número inteiro:"));
+const numero2 = parseInt(prompt("Digite o segundo número inteiro:"));
+const numero3 = parseInt(prompt("Digite o terceiro número inteiro:"));
+
+let maiorNumero;
+if (numero1 > numero2 && numero1 > numero3) {
+  maiorNumero = numero1;
+} else if (numero2 > numero1 && numero2 > numero3) {
+  maiorNumero = numero2;
+} else {
+  maiorNumero = numero3;
+}
+
+console.log("O maior número é: " + maiorNumero)
+
+let menorNumero;
+if (numero1 < numero2 && numero1 < numero3) {
+  menorNumero = numero1;
+} else if (numero2 < numero1 && numero2 < numero3) {
+  menorNumero = numero2;
+} else {
+  menorNumero = numero3;
+}
+
+console.log("O menor número é: " + menorNumero)
